a2a2cae05a4de9b0b9620478aa329452f3ea829a
TryingPop <qotjdgns2001@gmail.com>
2023-01-21 22:51:53 +0900

    feat(std) : 2023. 1 Study

---
+++ Study/01_/01_입출력.cpp
@@ -0,0 +1,23 @@
+﻿// 연습용_001.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>		// 기본 입출력 시스템을 담은 헤더
+using namespace std;	// cin, cout을 간단하게 치기위해 앞에 선언
+/*
+int main()
+{
+
+	// 담을 변수 동적 선언
+	string inputStr;
+
+	// 문자 출력
+	cout << "원하는 문자를 입력해주세요 : ";
+
+	// 입력 받은 문자열을 inputStr에 입력
+	// 현재 string으로 입력받고 넣기에 이상없이 들어간다
+	cin >> inputStr;
+
+	//입력한 문자를 보여준다
+	cout << "입력하신 문자는 " << inputStr << "입니다.";
+}
+*/
