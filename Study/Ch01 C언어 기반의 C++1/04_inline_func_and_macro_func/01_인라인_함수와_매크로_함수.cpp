612b32b4c24aced4532981913176c872ef3d8692
TryingPop <qotjdgns2001@gmail.com>
2023-01-22 15:36:57 +0900

    feat(oop) : 2023. 1 Study

--- Study/Ch01 C언어 기반의 C++1/04_inline_func_and_macro_func/01_인라인_함수와_매크로_함수.cpp
+++ Study/Ch01 C언어 기반의 C++1/04_inline_func_and_macro_func/01_인라인_함수와_매크로_함수.cpp
@@ -10,12 +10,25 @@ using namespace std;
 	매크로 함수 : 일반 함수에 비해 속도가 빠르다 
 			반면 복잡한 함수를 매크로의 형태로 정의하는데 한계가 있다
 	인라인 함수 : 매크로 함수의 단점을 보완한 C++의 함수
+	
+	매크로 함수는 자료형에 상관없이 사용가능하다
+	인라인 함수는 템플릿을 이용해 자료형 상관없이 이용 가능
+
+	템플릿은 다음과 같이 이용 가능 추후에 다룬다
+	template <typename T>
+	inline  T Add(T num1, T num2) {
+
+		return num1 + num2;
+	}
 */
 
 inline int Add(int num1, int num2) {
 
 	return num1 + num2;
 }
+
+
+
 
 int main()
 {
