2e6f591eeadcea02392d49437cf465fa86706381
TryingPop <qotjdgns2001@gmail.com>
2023-01-24 00:52:49 +0900

    feat(malloc) : 2023. 1 Study

---
+++ Study/Ch02 C언어 기반의 C++ 2/05_new_and_delete/01_malloc_and_free.cpp
@@ -0,0 +1,39 @@
+/*
+날짜 : 2023. 1. 24
+이름 : 배성훈
+내용 : malloc & free
+	C에서 사용하던 동적 메모리 할당
+*/
+
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+#define _CRT_SECURE_NO_WARNINGS		// strcpy 에러무시
+
+using namespace std;
+
+/// <summary>
+/// 문자열 동적 메모리 할당
+/// 1(char의 크기) * len 바이트만큼 할당한다
+/// </summary>
+/// <param name="len">문자열의 길이</param>
+/// <returns>할당된 메모리의 주소</returns>
+char* MakeStrAdr(int len) {
+
+	char* str = (char*)malloc(sizeof(char) * len);	
+	return str;
+}
+
+int main(void) {
+
+	// str 포인터에 20 바이트 메모리 할당
+	char* str = MakeStrAdr(20);
+
+	// str에 I am so happy~ 문자열을 복사해 넣는다
+	strcpy(str, "I am so happy~");
+	cout << str << endl;
+	// str 메모리 해제
+	free(str);
+	return 0;
+}
