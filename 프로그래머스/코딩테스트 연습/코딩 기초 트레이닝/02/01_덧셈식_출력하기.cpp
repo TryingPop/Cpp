bb83fd501c63f878059bf46dc6966500cbed01c7
TryingPop <qotjdgns2001@gmail.com>
2024-02-08 10:24:07 +0900

    feat(cs) : 2024. 2 Study Day2 출력과 연산
    기초문제이다
    string에서 문자열 하나씩 접근할 때는,
    string str;
    for (char c : str) {}
    로 할 수 있다
    
    그리고, replace 함수를 이용해 원하는 부분을 바꿀 수도 있다

---
+++ 프로그래머스/코딩테스트 연습/코딩 기초 트레이닝/02/01_덧셈식_출력하기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/*
+날짜 : 2024. 2. 8
+이름 : 배성훈
+내용 : 덧셈식 출력하기
+	특정 형태로 문자열 출력하기
+*/
+
+using namespace std;
+
+int main1(void)
+{
+
+	int a;
+	int b;
+	
+	cin >> a >> b;
+	cout << a << " + " << b << " = " << a + b << endl;
+	return 0;
+}
