b024561d6ad88997fbd1ce3f8a6c6c52c32268d5
TryingPop <qotjdgns2001@gmail.com>
2024-03-07 13:49:14 +0900

    feat(test) : 2024. 3 Study Day1 사칙연산

---
+++ 프로그래머스/코딩테스트 연습/코딩 입문 트레이닝/01/03_두_수의_곱.cpp
@@ -0,0 +1,16 @@
+/*
+날짜 : 2024. 3. 7
+이름 : 배성훈
+내용 : 두 수의 곱
+	정수 num1, num2가 주어질 때,
+	num1과 num2를 곱한 값을
+	return 하는 solution 함수 구현
+*/
+
+int solution(int _num1, int _num2)
+{
+
+	int answer = _num1 * _num2;
+
+	return answer;
+}
---
+++ 프로그래머스/코딩테스트 연습/코딩 입문 트레이닝/01/04_몫_구하기.cpp
@@ -0,0 +1,16 @@
+/*
+날짜 : 2024. 3. 7
+이름 : 배성훈
+내용 : 몫 구하기
+	정수 num1, num2가 주어질 때,
+	num1을 num2로 나눈 몫을
+	return하는 soltuion 함수 구현
+*/
+
+int solution(int _num1, int _num2)
+{
+
+	int answer = _num1 / _num2;
+
+	return answer;
+}
