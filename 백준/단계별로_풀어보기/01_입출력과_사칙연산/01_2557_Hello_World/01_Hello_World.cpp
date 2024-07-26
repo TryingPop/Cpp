8a6e44df83d2e40a8b68dd1c309947507f73ac19
TryingPop <qotjdgns2001@gmail.com>
2024-07-27 08:47:17 +0900

    feat: 2557번 Hello World 문제 2024. 7 백준 단계별로 풀어보기
    iostream 헤더 파일의 cout 을 이용해 정답을 출력한다

---
+++ 백준/단계별로_풀어보기/01_입출력과_사칙연산/01_2557_Hello_World/01_Hello_World.cpp
@@ -0,0 +1,19 @@
+/*
+날짜 : 2024. 7. 27
+이름 : 배성훈
+내용 : Hello World
+	문제번호 : 2557번
+
+	구현 문제다
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+	cout << "Hello World!";
+	return 0;
+}
