47fa33ea4e6172545b8cfaf4a26aaf1323880438
TryingPop <qotjdgns2001@gmail.com>
2024-07-27 00:54:45 +0900

    feat: lru 알고리즘 2024. 7 Study
    프로그래머스 lvl 2 캐시 문제에서 사용한 lru 알고리즘을
    템플릿을 이용해 더 많은 자료형으로 확장 시킨 코드다
    
    그리고 LRU 클래스의 GetRet메서드를 AddData로
    작동하는 기능으로 이름 변경
    
    AddData의 내부 값 5는 CACHE HIT 를 매크로 상수와
    1은 CACHE MISS 매크로 상수로 변경했다

---
+++ 알고리즘/01_LRU/01_MyLRU.cpp
@@ -0,0 +1,135 @@
+#include <cstdlib>
+#include <unordered_map>
+
+#define CACHE_HIT 5
+#define CACHE_MISS 1
+
+using namespace std;
+
+class Node
+{
+
+private:
+    bool use;   // 현재 사용 여부
+    Node* prev; // 이전 노드
+    Node* next; // 다음 노드
+
+public:
+    Node() : use(false), prev(NULL), next(NULL) { }
+
+    void Connect(Node* _head)
+    {
+
+        // 노드 잇기
+        // 널 체크 확인하면 안정성 올라간다
+        if (use) DisConnect();
+
+        Node* n = _head->next;
+        n->prev = this;
+        _head->next = this;
+
+        this->prev = _head;
+        this->next = n;
+        use = true;
+    }
+
+    void DisConnect()
+    {
+
+        // 노드 끊기
+        Node* n = this->next;
+        Node* p = this->prev;
+
+        n->prev = p;
+        p->next = n;
+
+        this->use = false;
+    }
+
+    void SetPrev(Node* _prev)
+    {
+
+        prev = _prev;
+    }
+
+    void SetNext(Node* _next)
+    {
+
+        next = _next;
+    }
+
+    Node* GetPrev()
+    {
+
+        return this->prev;
+    }
+
+    Node* GetNext()
+    {
+
+        return this->next;
+    }
+
+    bool Use()
+    {
+
+        return use;
+    }
+};
+
+
+template <typename T> class LRU
+{
+
+private:
+    unordered_map<T, Node*> data;   // 원소에 해당하는 노드 사용중인지 확인
+    Node* head;                     // 앞에 이어 붙이기 용도
+    Node* tail;                     // 마지막 노드 뺄 용도
+
+    int len;
+    int capacity;
+public:
+    LRU(int _capacity) : len(0), capacity(_capacity)
+    {
+
+        head = new Node();
+        tail = new Node();
+
+        head->SetNext(tail);
+        tail->SetPrev(head);
+    }
+
+
+    int AddData(T& _ele)
+    {
+
+        if (!data.count(_ele))
+        {
+
+            Node* temp = new Node();
+            data[_ele] = temp;
+        }
+
+        // 데이터 추가 확인
+        int ret = CACHE_HIT;
+        // 캐시에 데이터가 없는지 확인
+        if (!data[_ele]->Use()) len++;
+        // 캐시에 데이터가 존재
+        else ret = CACHE_MISS;
+
+        data[_ele]->Connect(head);
+
+        // 캐시가 가득차서 빼야하는지 확인
+        // 원래는 가득 찼는지 확인해야 하지만,
+        // linked list로 자료구조를 선정해서 뒤에 연산해도 이상없다
+        if (capacity < len)
+        {
+
+            tail->GetPrev()->DisConnect();
+            len--;
+        }
+
+        // 캐시 히트인지 캐시 미스인지 반환
+        return ret;
+    }
+};
