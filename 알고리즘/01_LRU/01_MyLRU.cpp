776c07a6931c1759058839251f8c4e4cfff5a54e
TryingPop <qotjdgns2001@gmail.com>
2024-07-28 00:00:31 +0900

    fix: LRU 알고리즘 2024. 7 Study
    노드 수정
    키 데이터 추가
    이에 따라 노드도 템플릿으로 변경
    null 체크해서 코드 안정성을 올렸다
    next, prev는 따로 설정 못하게 막아뒀다
    
    LRU 수정
    ChkLength 메서드 이름을 ChkCapcity로 용량 체크로 바꿨다
    Add Data역시 명확하지 않아 Add로 바꾸고
    cachemiss, cachehit 판별은 ChkHit라는 메서드를 만들어 확인했다
    길이초과에 따른, 노드 제거함수 Remove 추가
    입력으로 인한 노드 제거만 있어 크기 1짜리 풀을 둬서 재활용

--- 알고리즘/01_LRU/01_MyLRU.cpp
+++ 알고리즘/01_LRU/01_MyLRU.cpp
@@ -1,18 +1,19 @@
 #include <cstdlib>
 #include <unordered_map>
 
-#define CACHE_HIT 5
-#define CACHE_MISS 1
+#define CACHE_HIT 1
+#define CACHE_MISS 5
 
 using namespace std;
 
-class Node
+template <typename T> class Node
 {
 
 private:
-    bool use;   // 현재 사용 여부
     Node* prev; // 이전 노드
     Node* next; // 다음 노드
+    T key;      // 노드가 가리키는 정보
+    bool use;   // 현재 사용 여부
 
 public:
     Node() : use(false), prev(NULL), next(NULL) { }
@@ -21,15 +22,19 @@ public:
     {
 
         // 노드 잇기
-        // 널 체크 확인하면 안정성 올라간다
         if (use) DisConnect();
 
         Node* n = _head->next;
-        n->prev = this;
-        _head->next = this;
+        if (n != NULL)
+        {
 
+            n->prev = this;
+            this->next = n;
+        }
+
+        _head->next = this;
         this->prev = _head;
-        this->next = n;
+        
         use = true;
     }
 
@@ -40,106 +45,145 @@ public:
         Node* n = this->next;
         Node* p = this->prev;
 
-        n->prev = p;
-        p->next = n;
+        this->next = NULL;
+        this->prev = NULL;
+
+        if (n != NULL) n->prev = p;
+        if (p != NULL) p->next = n;
 
         this->use = false;
     }
 
-    void SetPrev(Node* _prev)
-    {
+    Node* GetPrev() { return this->prev; }
+    Node* GetNext() { return this->next; }
 
-        prev = _prev;
-    }
+    void SetKey(T _key) { this->key = _key; }
+    T GetKey() { return this->key; }
 
-    void SetNext(Node* _next)
-    {
-
-        next = _next;
-    }
-
-    Node* GetPrev()
-    {
-
-        return this->prev;
-    }
-
-    Node* GetNext()
-    {
-
-        return this->next;
-    }
-
-    bool Use()
-    {
-
-        return use;
-    }
+    bool Use() { return use; }
 };
-
 
 template <typename T> class LRU
 {
 
 private:
-    unordered_map<T, Node*> data;   // 원소에 해당하는 노드 사용중인지 확인
-    Node* head;                     // 앞에 이어 붙이기 용도
-    Node* tail;                     // 마지막 노드 뺄 용도
+    unordered_map<T, Node<T>*> data;    // 원소에 해당하는 노드 사용중인지 확인
+    Node<T>* head;                      // 앞에 이어 붙이기 용도
+    Node<T>* tail;                      // 마지막 노드 뺄 용도
+
+    Node<T>* pool;                      // 재활용
 
     int len;
     int capacity;
-public:
-    LRU(int _capacity) : len(0), capacity(_capacity)
+
+    Node<T>* Pooling()
     {
 
-        head = new Node();
-        tail = new Node();
-
-        head->SetNext(tail);
-        tail->SetPrev(head);
-    }
-
-    void RegisterNode(T& _ele)
-    {
-
-        // data에 등록된 노드인지 확인
-        // 없다면 생성
-        if (data.count(_ele)) return;
-
-        Node* temp = new Node();
-        data[_ele] = temp;
-    }
-
-    void ChkLength() 
-    {
-
-        // 캐시가 가득차서 빼야하는지 확인
-        // 원래는 가득 찼는지 확인해야 하지만,
-        // linked list로 자료구조를 선정해서 뒤에 연산해도 이상없다
-        if (capacity >= len) return;
-
-        tail->GetPrev()->DisConnect();
-        len--;
-    }
-
-    int AddData(T& _ele)
-    {
-
-        RegisterNode(_ele);
-
-        // 데이터 추가 확인
-        int ret = CACHE_HIT;
-        // 캐시에 데이터가 없는지 확인
-        if (!data[_ele]->Use()) len++;
-        // 캐시에 데이터가 존재
-        else ret = CACHE_MISS;
-
-        // 해당 데이터 맨 앞으로
-        data[_ele]->Connect(head);
-
-        ChkLength();
-
-        // 캐시 히트인지 캐시 미스인지 반환
+        if (pool == NULL) return new Node<T>();
+        Node<T>* ret = pool;
+        pool = NULL;
         return ret;
     }
+
+    void ChkCapacity()
+    {
+
+        /*
+        
+        용량 확인
+        */
+        if (len <= capacity) return;
+
+        Node<T>* remove = tail->GetPrev();
+        while (capacity < len && remove != head)
+        {
+
+            Remove(remove->GetKey());
+            remove = remove->GetPrev();
+            len--;
+        }
+    }
+
+public:
+    LRU(int _capacity) : len(0), capacity(_capacity), pool(NULL)
+    {
+
+        head = new Node<T>();
+        tail = new Node<T>();
+
+        tail->Connect(head);
+    }
+
+    void Remove(const T& _ele) 
+    {
+
+        /* 
+        
+        노드 제거
+        */
+
+        Node<T>* remove = data[_ele];
+
+        data.erase(remove->GetKey());
+        remove->DisConnect();
+
+        // 제거한거 pool이 비워져있다면 저장
+        if(pool == NULL) pool = remove;
+    }
+
+    void Add(const T& _ele) 
+    {
+
+        if (data.count(_ele)) 
+        {
+
+            // 이미 있는거면 앞으로 이동만한다
+            Node<T>* cur = data[_ele];
+            cur->Connect(head);
+
+            return;
+        }
+
+        Node<T>* add = Pooling();
+        add->Connect(head);
+        add->SetKey(_ele);
+        data[_ele] = add;
+        len++;
+
+        ChkCapacity();
+    }
+
+    int ChkHit(const T& _ele)
+    {
+        
+        /*
+        
+        캐시 히트인지, 캐시 미스인지 반환한다
+        */
+
+        if (data.count(_ele)) return CACHE_HIT;
+        else return CACHE_MISS;
+    }
 };
+
+#include <vector>
+#include <iostream>
+
+int main(void)
+{
+
+    vector<string> input = { "A", "B", "C", "D", "B" };
+    int cacheSize = 3;
+    LRU<string> lru(cacheSize);
+
+    int answer = 0;
+    for (string& str : input)
+    {
+
+        answer += lru.ChkHit(str);
+        lru.Add(str);
+    }
+
+    cout << answer;                     // 21
+    return 0;
+}
