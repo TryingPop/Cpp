6b1f387cff80c62f1f8029189ae155e0bcf269fa
TryingPop <qotjdgns2001@gmail.com>
2024-07-27 17:02:33 +0900

    feat: 10998번 A x B 문제 2024. 7 백준 단계별로 풀어보기 - 입출력과 사칙연산 단원
    iostream 헤더파일을 cin 을 이용해 입력받고
    cout을 이용해 출력한다

---
+++ 백준/단계별로_풀어보기/01_입출력과_사칙연산/04_10998_AxB/04_AxB.cpp
@@ -0,0 +1,22 @@
+/*
+날짜 : 2024. 7. 27
+이름 : 배성훈
+내용 : AxB
+	문제번호 : 10998번
+
+	수학, 구현, 사칙연산 문제다
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+
+	return 0;
+}
