ba5f2f5f64b28b40b8ee4c0c0fbab7ab40698026
TryingPop <qotjdgns2001@gmail.com>
2024-07-28 18:03:50 +0900

    feat: 10869번 사칙연산 문제 2024. 7 백준 단계별로 풀어보기 - 입출력과 사칙연산 단원
    iostream 헤더파일의 cin을 이용해 입력받고
    cout을 이용해 출력한다

---
+++ 백준/단계별로_풀어보기/01_입출력과_사칙연산/06_10869_사칙연산/01_사칙연산.cpp
@@ -0,0 +1,29 @@
+/*
+날짜 : 2024. 7. 28
+이름 : 배성훈
+내용 : 사칙연산
+	문제번호 : 10869번
+
+	수학, 구현, 사칙연산 문제다
+*/
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main(void)
+{
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+
+	return 0;
+}
