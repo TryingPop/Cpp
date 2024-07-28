32abfa984d256940a2942a67a0d46464f91590d8
TryingPop <qotjdgns2001@gmail.com>
2024-07-28 18:13:57 +0900

    feat: 10926번 ??! 문제 2024. 7 백준 단계별로 풀어보기 - 입출력과 사칙연산 단원
    iostream 헤더파일의 cin을 이용해 입력받고
    cout을 이용해 출력한다
    
    문자열은 string 헤더파일을 통해 입력받는다
    (string.h와 다른 헤더파일이다)

---
+++ 백준/단계별로_풀어보기/01_입출력과_사칙연산/07_10926_？？！/02_？？！.cpp
@@ -0,0 +1,23 @@
+/*
+날짜 : 2024. 7. 28
+이름 : 배성훈
+내용 : ??!
+	문제번호 : 10926번
+
+	구현 문제다
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+
+	string input;
+	cin >> input;
+
+	cout << input << "??!";
+	return 0;
+}
