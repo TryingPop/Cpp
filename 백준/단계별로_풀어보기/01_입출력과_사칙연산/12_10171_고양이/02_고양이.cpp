0c5c8008ba290e26e7bcc8b6433f7da257cf42b5
TryingPop <qotjdgns2001@gmail.com>
2024-07-29 09:51:35 +0900

    feat: 10171번 고양이 문제 2024. 7 백준 단계별로 풀어보기 - 입출력과 사칙연산 단원
    특수문자를 출력하는 문제다

---
+++ 백준/단계별로_풀어보기/01_입출력과_사칙연산/12_10171_고양이/02_고양이.cpp
@@ -0,0 +1,25 @@
+/*
+날짜 : 2024. 7. 29
+이름 : 배성훈
+내용 : 고양이
+	문제번호 : 10171번
+
+	구현 문제다
+*/
+
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main(void)
+{
+
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+
+	return 0;
+}
