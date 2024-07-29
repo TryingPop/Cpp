457e70bda78fe486bd0cf14decc91fb46a8e5e66
TryingPop <qotjdgns2001@gmail.com>
2024-07-29 09:53:32 +0900

    feat: 11382번 꼬마 정민 문제 2024. 7 백준 단계별로 풀어보기 - 입출력과 사칙연산 단원
    long long 범위를 입력받는 문제다
    cin은 다양한 타입을 받을 수 있어 자료형만 신경쓰면 된다

---
+++ 백준/단계별로_풀어보기/01_입출력과_사칙연산/11_11382_꼬마_정민/01_꼬마_정민.cpp
@@ -0,0 +1,23 @@
+/*
+날짜 : 2024. 7. 29
+이름 : 배성훈
+내용 : 꼬마 정민
+	문제번호 : 11382번
+
+	수학, 구현, 사칙연산 문제다
+*/
+
+#include <iostream>
+
+#define ll long long
+using namespace std;
+
+int main(void)
+{
+
+	ll a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+	return 0;
+}
