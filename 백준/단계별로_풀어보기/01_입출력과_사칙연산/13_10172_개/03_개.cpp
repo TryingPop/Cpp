5df4a942579ef14b407727f52889a1fb94e240a9
TryingPop <qotjdgns2001@gmail.com>
2024-07-29 10:00:49 +0900

    feat: 10172번 개 문제 2024. 7 백준 단계별로 풀어보기 - 입출력과 사칙연산
    특수문자를 출력하는 문제다

---
+++ 백준/단계별로_풀어보기/01_입출력과_사칙연산/13_10172_개/03_개.cpp
@@ -0,0 +1,25 @@
+/*
+날짜 : 2024. 7. 29
+이름 : 배성훈
+내용 : 개
+	문제번호 : 10172번
+
+	구현 문제다
+*/
+
+#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+int main(void)
+{
+
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|";
+
+	return 0;
+}
