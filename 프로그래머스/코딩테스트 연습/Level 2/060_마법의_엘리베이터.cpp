02ac7a5fdbec33c98c3f114eccc58068dfa4d53e
TryingPop <qotjdgns2001@gmail.com>
2024-08-04 13:41:22 +0900

    feat: 마법의 엘리베이터 2024. 8 프로그래머스 lvl 2
    브루트 포스 문제다
    입력값이 10^8이고
    자리수별로 2회씩 진행하기에
    많아야 2^8번 시행한다
    
    그래서 dfs 탐색방법으로 모든 경우를 탐색했다

---
+++ 프로그래머스/코딩테스트 연습/Level 2/060_마법의_엘리베이터.cpp
@@ -0,0 +1,64 @@
+/*
+날짜 : 2024. 8. 4
+이름 : 배성훈
+내용 : 마법의 엘리베이터
+	마법의 엘리베이터의 버튼은 특별하다
+	마법의 엘리베이터에는 -1, +1, -100, +100 등과 같이
+	절대값이 10^c(c >= 0 인 정수) 형태인 정수들이 적힌 버튼이 있다
+
+	마법의 엘리베이터의 버튼을 누르면 현재 층 수에 버튼에 
+	적혀 있는 값을 더한 층으로 이동하게 된다
+	
+	단, 엘리베이터가 위치해 있는 층과 버튼의 값을 더한 결과가 0보다 작으면
+	엘리베이터는 움직이지 않는다
+
+	최소한의 버튼을 눌러 0층으로 이동하려고 한다
+	현재 층수를 나타내는 storey가 주어졌을 때,
+	0층으로 가기 위해 필요한 마법의 돌의 최소값을
+	return 하는 solution 함수 구현
+
+	storey는 1억 이하의 자연수
+
+	parameter
+	16
+
+
+	return
+	6
+
+
+	sol
+	+1을 4번눌러 20층을 간 뒤
+	-10을 2번 눌러 0층으로 가는게 최소이다
+*/
+
+inline int DFS(int _val)
+{
+
+	if (_val == 0) return 0;
+
+	int cur = _val % 10;
+	int next = _val / 10;
+
+	int ret = DFS(next) + cur;
+
+	if (next || cur > 5)
+	{
+
+		int chk = DFS(next + 1) + (10 - cur);
+		if (chk < ret) ret = chk;
+	}
+
+	return ret;
+}
+
+int solution(int _storey)
+{
+
+	/*
+	
+	브루트 포스
+	*/
+	int answer = DFS(_storey);
+	return answer;
+}
