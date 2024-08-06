d32cc83e1e4b4e6611696d7a3ca8194423bc0c09
TryingPop <qotjdgns2001@gmail.com>
2024-08-06 19:22:01 +0900

    feat: 11654번 아스키 코드 문제 2024. 8 백준 단계별로 풀어보기 - 문자열
    char로 입력받으면 아스키 코드로 입력받기에
    char를 다시 int로 변환해 반환하면 된다

---
+++ 백준/단계별로_풀어보기/05_문자열/04_11654_아스키_코드/04_아스키_코드.cpp
@@ -0,0 +1,21 @@
+/*
+날짜 : 2024. 8. 6
+이름 : 배성훈
+내용 : 아스키 코드
+	문제번호 : 11654번
+
+	구현 문제다
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+	char c;
+	cin >> c;
+	cout << (int)c;
+	return 0;
+}
