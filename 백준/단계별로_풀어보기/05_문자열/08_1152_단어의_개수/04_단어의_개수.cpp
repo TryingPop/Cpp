9443150437eab943e475b254315fb81c19aa18cf
TryingPop <qotjdgns2001@gmail.com>
2024-08-07 19:04:33 +0900

    fix: 1152번 단어의 개수 문제 2024. 8 백준 단계별로 풀어보기 - 문자열
    cin.get()으로 한개씩 읽으며 해결했다

--- 백준/단계별로_풀어보기/05_문자열/08_1152_단어의_개수/04_단어의_개수.cpp
+++ 백준/단계별로_풀어보기/05_문자열/08_1152_단어의_개수/04_단어의_개수.cpp
@@ -24,12 +24,36 @@ int main(void)
 
 	int ret = 0;
 
+#if first
 	while (cin >> str)
 	{
 
 		ret++;
 	}
+#else
 
+	while (true)
+	{
+
+		char c = cin.get();
+		if (c == ' ')
+		{
+
+			if (str == "") continue;
+			ret++;
+			str.clear();
+			continue;
+		}
+		else if (c == EOF || c == '\r' || c == '\n') 
+		{ 
+			
+			if (str != "") ret++;
+			break; 
+		}
+
+		str.push_back(c);
+	}
+#endif
 	cout << ret;
 	return 0;
 }
