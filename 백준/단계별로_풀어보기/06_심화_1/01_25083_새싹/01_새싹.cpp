d473d0d09e6990d5b90b8df8cb865b0d05b9bad6
TryingPop <qotjdgns2001@gmail.com>
2024-08-09 17:39:08 +0900

    feat: 25083번 새싹 문제 2024. 8 백준 단계별로 풀어보기 - 심화 1
    출력형식에 맞춰 출력하는 구현 문제다

---
+++ 백준/단계별로_풀어보기/06_심화_1/01_25083_새싹/01_새싹.cpp
@@ -0,0 +1,24 @@
+/*
+날짜 : 2024. 8. 9
+이름 : 배성훈
+내용 : 새싹
+	문제번호 : 25083번
+
+	구현 문제다
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+	cout << "         ,r\'\"7\n";
+	cout << "r`-_   ,\'  ,/\n";
+	cout << " \\. \". L_r\'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+	return 0;
+}
