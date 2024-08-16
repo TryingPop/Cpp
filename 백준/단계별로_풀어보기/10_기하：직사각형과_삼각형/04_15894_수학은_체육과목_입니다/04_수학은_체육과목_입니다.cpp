8115cd2301d0fc3e5a66c278aab3f96fcfc0b9af
TryingPop <qotjdgns2001@gmail.com>
2024-08-16 14:07:55 +0900

    feat: 15894번 수학은 체육과목 입니다 문제 2024. 8 백준 단계별로 풀어보기 - 기하: 직사각형과 삼각형
    선분을 평행이동 시켜 둘레의 길이를 구하는 문제다
    10^9까지 주어지기에 최대값은 int 범위를 벗어날 수 있다
    그래서 long long 자료형을 이용했다

---
+++ 백준/단계별로_풀어보기/10_기하：직사각형과_삼각형/04_15894_수학은_체육과목_입니다/04_수학은_체육과목_입니다.cpp
@@ -0,0 +1,22 @@
+/*
+날짜 : 2024. 8. 16
+이름 : 배성훈
+내용 : 수학은 체육과목 입니다
+	문제번호 : 15894번
+
+	수학, 사칙연산 문제다
+*/
+
+#include <iostream>
+
+#define ll long long
+using namespace std;
+
+int main(void)
+{
+
+	ll x;
+	cin >> x;
+	cout << x * 4;
+	return 0;
+}
