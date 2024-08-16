be1d1b5738056dad5e3e437040d737dd15a9dd0f
TryingPop <qotjdgns2001@gmail.com>
2024-08-16 13:52:07 +0900

    feat: 27323번 직사각형 문제 2024. 8 백준 단계별로 풀어보기 - 기하: 직사각형과 삼각혁
    넓이 구하는 문제다

---
+++ 백준/단계별로_풀어보기/10_기하：직사각형과_삼각형/01_27323_직사각형/01_직사각형.cpp
@@ -0,0 +1,21 @@
+/*
+날짜 : 2024. 8. 16
+이름 : 배성훈
+내용 : 직사각형
+	문제번호 : 27323번
+
+	수학, 기하학, 사칙연산 문제다
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+	int w, h;
+	cin >> w >> h;
+	cout << w * h;
+	return 0;
+}
