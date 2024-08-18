a72c7aefb9d325b205c8c40dc8f86526f4c4020e
TryingPop <qotjdgns2001@gmail.com>
2024-08-18 23:10:46 +0900

    feat: 24266번 알고리즘 수업 - 알고리즘의 수행 시간 5 문제 2024. 8 백준 단계별로 풀어보기 - 시간 복잡도
    간단한 구현 문제다

---
+++ 백준/단계별로_풀어보기/11_시간_복잡도/05_24266_알고리즘_수업_-_알고리즘_수행_시간_5/01_알고리즘_수업_-_알고리즘의_수행_시간_5.cpp
@@ -0,0 +1,25 @@
+/*
+날짜 : 2024. 8. 19
+이름 : 배성훈
+내용 : 알고리즘 수업 - 알고리즘의 수행 시간 5
+	문제번호 : 24266번
+
+	수학, 구현, 시뮬레이션 문제다
+*/
+
+#include <iostream>
+
+#define ll long long
+#define endl	'\n'
+
+using namespace std;
+
+int main(void)
+{
+
+	ll n;
+	cin >> n;
+
+	cout << n * n * n << endl << 3;
+	return 0;
+}
