1e05457e772755364fa070f64a2d936cf1f33295
TryingPop <qotjdgns2001@gmail.com>
2024-08-18 14:54:32 +0900

    feat: 24263번 알고리즘 수업 - 알고리즘의 수행 시간 2 문제 2024. 8 백준 단계별로 풀어보기 - 시간 복잡도
    총합 구하는 함수의 시간복잡도를 계산하는 문제다

---
+++ 백준/단계별로_풀어보기/11_시간_복잡도/02_24263_알고리즘_수업_-_알고리즘_수행_시간_2/02_알고리즘_수업_-_알고리즘의_수행_시간_2.cpp
@@ -0,0 +1,23 @@
+/*
+날짜 : 2024. 8. 18
+이름 : 배성훈
+내용 : 알고리즘 수업 - 알고리즘의 수행 시간 2
+	문제번호 : 24263번
+
+	구현, 시뮬레이션 문제다
+*/
+
+#include <iostream>
+
+#define endl	'\n'
+using namespace std;
+
+int main(void)
+{
+
+	int n;
+
+	cin >> n;
+	cout << n << endl << 1;
+	return 0;
+}
