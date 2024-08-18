36f5c845e0c9b584dd08085f523c6f21bba41e4c
TryingPop <qotjdgns2001@gmail.com>
2024-08-18 14:59:23 +0900

    feat: 24264번 알고리즘 수업 - 알고리즘의 수행 시간 3 문제 2024. 8 백준 단계별로 풀어보기 - 시간 복잡도
    2중 포문의 시간복잡도를 구하는 문제다

---
+++ 백준/단계별로_풀어보기/11_시간_복잡도/03_24264_알고리즘_수업_-_알고리즘_수행_시간_3/03_알고리즘_수업_-_알고리즘의_수행_시간_3.cpp
@@ -0,0 +1,24 @@
+/*
+날짜 : 2024. 8. 18
+이름 : 배성훈
+내용 : 알고리즘 수업 - 알고리즘의 수행 시간 3
+	문제번호 : 24264번
+
+	수학, 구현, 사칙연산, 시뮬레이션 문제다
+*/
+
+#include <iostream>
+
+#define ll long long
+#define endl	'\n'
+using namespace std;
+
+int main(void)
+{
+
+	ll n;
+	
+	cin >> n;
+	cout << n * n << endl << 2;
+	return 0;
+}
