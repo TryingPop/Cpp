0989ede3caf0503f8503f4104c52319253c67436
TryingPop <qotjdgns2001@gmail.com>
2024-08-18 14:48:42 +0900

    feat: 24262번 알고리즘 수업 - 알고리즘 수행 시간 1 문제 2024. 8 백준 단계별로 풀어보기 - 시간 복잡도
    연산이 몇 번 진행했는지 확인하고
    시간복잡도를 확인하는 문제다

---
+++ 백준/단계별로_풀어보기/11_시간_복잡도/01_24262_알고리즘_수업_-_알고리즘_수행_시간_1/01_알고리즘_수업_-_알고리즘의_수행_시간_1.cpp
@@ -0,0 +1,20 @@
+/*
+날짜 : 2024. 8. 18
+이름 : 배성훈
+내용 : 알고리즘 수업 - 알고리즘의 수행시간
+	문제번호 : 24262번
+
+	구현, 시뮬레이션 문제다
+*/
+
+#include <iostream>
+
+#define endl	'\n'
+using namespace std;
+
+int main(void)
+{
+
+	cout << 1 << endl << 0;
+	return 0;
+}
