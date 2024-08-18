23d52e65dd9d67b9a18d9176bf8612c228e043b5
TryingPop <qotjdgns2001@gmail.com>
2024-08-18 15:02:37 +0900

    feat: 24265번 알고리즘 수업 - 알고리즘 수행 시간 4 문제 2024. 8 백준 단계별로 풀어보기 - 시간 복잡도
    n - 1 + n - 2 + ... + 1 = (n x (n - 1)) / 2번 실행하고
    이는 n^2 의 시간복잡도가 된다

---
+++ 백준/단계별로_풀어보기/11_시간_복잡도/04_24265_알고리즘_수업_-_알고리즘_수행_시간_4/04_알고리즘_수업_-_알고리즘의_수행_시간_4.cpp
@@ -0,0 +1,24 @@
+/*
+날짜 : 2024. 8. 18
+이름 : 배성훈
+내용 : 알고리즘 수업 - 알고리즘의 수행 시간 4
+	문제번호 : 24265번
+
+	수학, 구현, 사칙연산, 시뮬레이션 문제다
+*/
+
+#include <iostream>
+
+#define ll		long long
+#define endl	'\n'
+using namespace std;
+
+int main(void)
+{
+
+	ll n;
+	cin >> n;
+
+	cout << (n * (n - 1)) / 2 << endl << 2;
+	return 0;
+}
