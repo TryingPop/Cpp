e57265a78c41c08540bc693353de8461356e931c
TryingPop <qotjdgns2001@gmail.com>
2024-08-29 22:07:32 +0900

    feat: 13909번 창문 닫기 문제 2024. 8 백준 단계별로 풀어보기 - 약수, 배수와 소수 2
    약수가 홀수개인 원소들만 켜진다
    약수가 홀수개인 원소는 제곱수 뿐이다
    그래서 제곱수의 개수를 구해 풀었다

---
+++ 백준/단계별로_풀어보기/15_약수,_배수와_소수_2/09_13909_창문_닫기/04_창문_닫기.cpp
@@ -0,0 +1,23 @@
+/*
+날짜 : 2024. 8. 29
+이름 : 배성훈
+내용 : 창문 닫기
+	문제번호 : 13909번
+
+	수학, 정수론 문제다
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+
+	int n;
+	cin >> n;
+
+	cout << (int)sqrt(n);
+	return 0;
+}
