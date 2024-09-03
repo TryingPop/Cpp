e2aa07b493807181e9b1152860a692f9869f3388
TryingPop <qotjdgns2001@gmail.com>
2024-09-03 22:17:10 +0900

    feat: 15439번 베라의 패션 문제 2024. 9 백준 단계별로 풀어보기 - 조합론
    nP2를 구하는 문제다

---
+++ 백준/단계별로_풀어보기/17_조합론/01_15439_베라의_패션/01_베라의_패션.cpp
@@ -0,0 +1,22 @@
+/*
+날짜 : 2024. 9. 3
+이름 : 배성훈
+내용 : 베라의 패션
+	문제번호 : 15439번
+
+	수학, 구현, 조합론 문제다
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+	int n;
+	cin >> n;
+
+	cout << n * (n - 1);
+	return 0;
+}
